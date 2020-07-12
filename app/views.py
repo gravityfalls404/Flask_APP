7e35369d3718fff622c45488e37a18b1671ef2f0
Prashant <prashantg.co.17@nsit.net.in>
2020-07-13 00:39:08 +0530

    SOlved Problems now working on a new branch

--- app/views.py
+++ app/views.py
@@ -69,3 +69,16 @@ def json():
 
         return res
 
+@app.route("/guestbook")
+def guestbook():
+    return render_template("/public/guestbook.html")
+
+@app.route("/guestbook/create-entry", methods = ["POST"])
+def create_entry():
+
+    req = request.get_json()
+    print(req)
+
+    res = make_response(jsonify({"message": "Json received"}), 200)
+
+    return req
